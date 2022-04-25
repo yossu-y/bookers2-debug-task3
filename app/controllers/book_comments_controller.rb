fd60517fb8017869f64201ee8336fe570e548663
yossu-y <yangyuub@gmail.com>
2022-04-25 06:20:42 +0000

    [add]viewページ

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,11 +1,15 @@
 class BookCommentsController < ApplicationController
+  before_action :correct_user, only: [:destroy]
+
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.user_id = current_user.id
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    if @book_comment.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      redirect_to book_path(@book), notice: "comment can't be blank"
+    end
   end
 
   def destroy
@@ -15,8 +19,16 @@ class BookCommentsController < ApplicationController
 
   private
 
-  def book_params
+  def book_comment_params
     params.require(:book_comment).permit(:comment)
+  end
+  
+  def correct_user
+    @book_comment = BookComment.find(params[:id])
+    @user = @book_comment.user
+    unless @user == current_user
+      redirect_to book_path(params[:book_id])
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
   get "home/about"=>"homes#about", as: "about"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy] 
-  resources :book_comments, only: [:create, :destroy] end
+  end
   
   resources :users, only: [:index,:show,:edit,:update]
 
